29c902c527807262cc35bcce27b09685140dca9e
DoMinhTin <dominhtin2006@gmail.com>
2026-08-30 12:46:53 +0700

    Add files via upload

---
+++ b7/b7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace b7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            long tong = 0;
+            long tongChan = 0;
+            long tongLe = 0;
+            long giaiThua = 1;
+            Console.WriteLine("MSSV:6551071084");
+            Console.Write("Nhap so nguyen duong n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                tong = tong + i;
+
+                if (i % 2 == 0)
+                {
+                    tongChan = tongChan + i;
+                }
+                else
+                {
+                    tongLe = tongLe + i;
+                }
+            }
+
+            int count = 1;
+            while (count <= n)
+            {
+                giaiThua = giaiThua * count;
+                count++;
+            }
+
+            Console.WriteLine("Tong 1 den n: " + tong);
+            Console.WriteLine("Tong cac so chan: " + tongChan);
+            Console.WriteLine("Tong cac so le: " + tongLe);
+            Console.WriteLine("Tich 1 den n (giai thua): " + giaiThua);
+        }
+    }
+}
---
+++ b8/b8/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace testc_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            int tong = 0;
+            int soChan = 0;
+            int soLe = 0;
+            Console.WriteLine("MSSV:6551071084");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Nhap phan tu thu " + i + ": ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Toan bo mang: ");
+            foreach (int item in a)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            int max = a[0];
+            int min = a[0];
+            int viTriMax = 0;
+            int viTriMin = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                tong = tong + a[i];
+
+                if (a[i] > max)
+                {
+                    max = a[i];
+                    viTriMax = i;
+                }
+
+                if (a[i] < min)
+                {
+                    min = a[i];
+                    viTriMin = i;
+                }
+
+                if (a[i] % 2 == 0)
+                {
+                    soChan++;
+                }
+                else
+                {
+                    soLe++;
+                }
+            }
+
+            double trungBinh = (double)tong / 10;
+
+            Console.WriteLine("Tong: " + tong);
+            Console.WriteLine("Trung binh: " + trungBinh);
+            Console.WriteLine("Gia tri lon nhat: " + max + " tai vi tri " + viTriMax);
+            Console.WriteLine("Gia tri nho nhat: " + min + " tai vi tri " + viTriMin);
+            Console.WriteLine("So luong phan tu chan: " + soChan);
+            Console.WriteLine("So luong phan tu le: " + soLe);
+
+            Console.Write("Mang sau khi dao nguoc: ");
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
