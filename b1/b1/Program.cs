2a9d45315c9274da9b4713c857597d2555cb470f
DoMinhTin <dominhtin2006@gmail.com>
2026-08-30 12:42:48 +0700

    Add files via upload

---
+++ b1/b1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace b1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hoTen;
+            int tuoi;
+            double chieuCao;
+            float canNang;
+            bool daTotNghiep;
+            Console.WriteLine("MSSV:6551071084");
+            Console.Write("Nhap ho ten: ");
+            hoTen = Console.ReadLine();
+
+            Console.Write("Nhap tuoi: ");
+            tuoi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhap chieu cao (m): ");
+            chieuCao = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhap can nang (kg): ");
+            canNang = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Da tot nghiep (true/false): ");
+            daTotNghiep = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("\n--- THONG TIN ---");
+            Console.WriteLine("Ho ten: " + hoTen);
+            Console.WriteLine("Tuoi: " + tuoi);
+            Console.WriteLine("Chieu cao: " + chieuCao + " m");
+            Console.WriteLine("Can nang: " + canNang + " kg");
+            Console.WriteLine("Da tot nghiep: " + daTotNghiep);
+        }
+    }
+}
