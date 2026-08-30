b74b3b91f9dfad491543e267f0bc16e0fd4ec1f3
DoMinhTin <dominhtin2006@gmail.com>
2026-08-30 12:43:58 +0700

    Add files via upload

---
+++ b2/b2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace b2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("MSSV:6551071084");
+            Console.Write("Nhap a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhap b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            Console.WriteLine(a + " - " + b + " = " + (a - b));
+            Console.WriteLine(a + " * " + b + " = " + (a * b));
+
+            if (b != 0)
+            {
+                Console.WriteLine(a + " / " + b + " = " + (a / b));
+                Console.WriteLine("Chia lay nguyen: " + ((int)a / (int)b));
+                Console.WriteLine("Chia lay du: " + ((int)a % (int)b));
+            }
+            else
+            {
+                Console.WriteLine("Khong the chia cho 0");
+            }
+
+            Console.WriteLine(a + "^" + b + " = " + Math.Pow(a, b));
+        }
+    }
+}
---
+++ b3/b3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace b3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal celsius, fahrenheit, kelvin;
+            Console.WriteLine("MSSV:6551071084");
+            Console.Write("Nhap do C: ");
+            celsius = Convert.ToDecimal(Console.ReadLine());
+
+            fahrenheit = celsius * 9 / 5 + 32;
+            kelvin = celsius + 273.15m;
+
+            Console.WriteLine("Fahrenheit: " + Math.Round(fahrenheit, 2) + " F");
+            Console.WriteLine("Kelvin: " + Math.Round(kelvin, 2) + " K");
+        }
+    }
+}
